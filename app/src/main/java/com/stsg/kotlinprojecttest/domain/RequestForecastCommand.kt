7537a68cdad517a8fbf4fe1dd2e02f404fe0f73b
wojtek <wojtek.developer@gmail.com>
2015-10-02 11:24:03 +0200

    done with changes

--- app/src/main/java/com/stsg/kotlinprojecttest/domain/RequestForecastCommand.kt
+++ app/src/main/java/com/stsg/kotlinprojecttest/domain/RequestForecastCommand.kt
@@ -1,5 +1,6 @@
 package com.stsg.kotlinprojecttest.domain
 
+import com.antonioleiva.weatherapp.domain.mappers.ForecastDataMapper
 import com.stsg.kotlinprojecttest.domain.domainmodel.ForecastList
 import com.stsg.kotlinprojecttest.network.ForecastRequest
 
@@ -7,11 +8,11 @@ import com.stsg.kotlinprojecttest.network.ForecastRequest
  * Created by wojtek on 18.09.15.
  *STSG POLAND ALL RIGHTS RESERVED
  */
-class RequestForecastCommand(private val zipCode: String) :
+class RequestForecastCommand(private val zipCode: Long) :
         Command<ForecastList> {
     override fun execute(): ForecastList {
         val forecastRequest = ForecastRequest(zipCode)
-        return ForecastDataMapper().convertFromDataModel(
+        return ForecastDataMapper().convertFromDataModel(zipCode,
                 forecastRequest.execute())
     }
 }
--- app/src/main/java/com/stsg/kotlinprojecttest/network/ForecastRequest.kt
+++ app/src/main/java/com/stsg/kotlinprojecttest/network/ForecastRequest.kt
@@ -8,16 +8,17 @@ import java.net.URL
 /**
  * Created by wojtek on 18.09.15.
  *STSG POLAND ALL RIGHTS RESERVED
- */
+*/
 
+class ForecastRequest(val zipCode: Long) {
 
- class ForecastRequest(val zipcode: String) {
     companion object {
-        private val url = "http://api.openweathermap.org/data/2.5/" +
+        private val URL = "http://api.openweathermap.org/data/2.5/" +
                 "forecast/daily?mode=json&units=metric&cnt=7&q="
     }
-     fun execute(): ForecastResult {
-        val forecastJsonStr = URL(url + zipcode).readText()
+
+    fun execute(): ForecastResult {
+        val forecastJsonStr = URL(URL + zipCode).readText()
         return Gson().fromJson(forecastJsonStr, ForecastResult::class.java)
     }
 }
